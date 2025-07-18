92d8e71451abcc730e21a0d0c667c63f759cca24
AdrianRp7 <parc769@gmail.com>
2025-07-18 12:54:57 +0200

    feat: This adds the hello world and the prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,41 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+    languageOptions: {
+        parserOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module'
+        }
+    },
+    plugins: {
+        prettier: eslintPluginPrettier
+    },
+    rules: {
+        'no-unused-vars': 'warn',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+
+        'vue/multi-word-component-names': 'off',
+
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                useTabs: false,
+                semi: true,
+                singleQuote: true,
+                printWidth: 100,
+                bracketSpacing: true,
+                arrowParens: 'avoid',
+                trailingComma: 'none',
+                htmlWhitespaceSensitivity: 'ignore',
+                vueIndentScriptAndStyle: true,
+                endOfLine: 'lf'
+            }
+        ]
+    },
+    ignores: ['dist', '.output', 'node_modules']
+});
