e94d488ed06260e66d52a0b8557d720ddab0f6f7
AdrianRp7 <parc769@gmail.com>
2025-07-19 19:42:34 +0200

    fix: add 's' to result in the interface ResponsePokemonList

--- interface/ResponsePokemonList.ts
+++ interface/ResponsePokemonList.ts
@@ -1,11 +1,14 @@
+import type { PokemonInterface } from './PokemonInterface';
+
 export interface ResponsePokemonList {
     count: number;
     next: string | null;
     previous: string | null;
-    result: PokemonNameUrl[];
+    results: PokemonNameUrl[];
 }
 
 export interface PokemonNameUrl {
     name: string;
     url: string;
+    pokemonData?: PokemonInterface;
 }
