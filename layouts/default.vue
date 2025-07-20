d2b31478ffd48e139c3ee8bed36dce74da3cd2bc
AdrianRp7 <parc769@gmail.com>
2025-07-20 16:21:25 +0200

    feat: add border box to all page and padding to the container layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col justify-content-between min-h-100">
         <SharedNavbar />
-        <main class="mx-auto container flex flex-1 h-100 py-4">
+        <main class="mx-auto container flex flex-1 h-100 py-4 px-2">
             <slot />
         </main>
         <SharedFooter />
