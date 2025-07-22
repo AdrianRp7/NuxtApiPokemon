7f2e56ffb647a597d01bf49fb9eb9dd6cdc3ddb3
AdrianRp7 <parc769@gmail.com>
2025-07-22 12:12:21 +0200

    feat: add a default imagen when the pokemon image is not loaded

--- components/pokemon/Card.vue
+++ components/pokemon/Card.vue
@@ -5,13 +5,25 @@
     >
         <article class="flex pokemon-card button-default button-default-animation h-100 p-4">
             <NuxtImg
+                v-slot="{ src, isLoaded, imgAttrs }"
                 :src="pokemonImage"
                 placeholder
                 fit="cover"
                 loading="lazy"
                 quality="80"
                 :alt="`Pokemon ${pokemon.name} image`"
-            />
+                :custom="true"
+            >
+                <!-- Show the actual image when loaded -->
+                <img v-if="isLoaded" v-bind="imgAttrs" :src="src" />
+
+                <!-- Show a placeholder while loading -->
+                <img
+                    v-else
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+                    alt="placeholder"
+                />
+            </NuxtImg>
             <div class="pokemon-card-data flex flex-col justify-content-between">
                 <h3 class="is-text-navigation capitalize title">{{ pokemon.name }}</h3>
                 <div>
